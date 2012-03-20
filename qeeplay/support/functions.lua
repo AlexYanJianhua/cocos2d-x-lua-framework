8437834290efcb614d97565e58d70524c3d10675
Liao YuLei <dualface@gmail.com>
2012-03-20 16:39:56 +0800

    add string.split()

--- qeeplay/support/functions.lua
+++ qeeplay/support/functions.lua
@@ -96,6 +96,15 @@ io.pathForFile = function(filename, path)
     return path .. "/" .. filename
 end
 
+---- string
+
+string.split = function(str, sep)
+    local sep, fields = sep or ":", {}
+    local pattern = string.format("([^%s]+)", sep)
+    string.gsub(str, pattern, function(c) fields[#fields+1] = c end)
+    return fields
+end
+
 
 ---- table
 
